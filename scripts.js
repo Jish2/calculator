a1fb50fca15698239ea82dc38c4d4f6aa36a3856
Joshua Goon <j.j.jappiecraft@gmail.com>
2021-12-31 11:08:45 -0600

    Almost finished!

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
-let display = 0;
+let curDis = 0;
+let currentNum = 0;
 let firstInput = 0;
 let secondInput = 0;
 
-document.getElementById("display").innerHTML = display;
+document.getElementById("display").innerHTML = curDis;
+
+const secret = () => {
+  if (document.getElementById("secret").style.display === "block") {
+    document.getElementById("secret").style.display = "none";
+  } else {
+    document.getElementById("secret").style.display = "block";
+  }
+};
 
 const add = (input1, input2) => {
   return input1 + input2;
@@ -29,46 +38,54 @@ const switchSign = (input) => {
 };
 
 const reset = () => {
-  console.log("pressed AC");
   document.getElementById("display").innerHTML = 0;
+  curDis = 0
+    currentNum = 0
 };
 
 const one = () => {
-  document.getElementsByClassName("display").innerHTML = 1;
+  if (display === 0) {
+      document.getElementById("display").innerHTML = 1;
+      curDis = 1
+      currentNum = 1
+} else {
+    console.log(document.getElementById("display").innerHTML + '1');
+    curDis = currentNum.toString + '1'
+    console.log(Number.parseInt(curDis,10)
 };
 
 const two = () => {
-  document.getElementsByClassName("display").innerHTML = 2;
+  document.getElementById("display").innerHTML = 2;
 };
 
 const three = () => {
-  document.getElementsByClassName("display").innerHTML = 3;
+  document.getElementById("display").innerHTML = 3;
 };
 
 const four = () => {
-  document.getElementsByClassName("display").innerHTML = 4;
+  document.getElementById("display").innerHTML = 4;
 };
 
 const five = () => {
-  document.getElementsByClassName("display").innerHTML = 5;
+  document.getElementById("display").innerHTML = 5;
 };
 
 const six = () => {
-  document.getElementsByClassName("display").innerHTML = 6;
+  document.getElementById("display").innerHTML = 6;
 };
 
 const seven = () => {
-  document.getElementsByClassName("display").innerHTML = 7;
+  document.getElementById("display").innerHTML = 7;
 };
 
 const eight = () => {
-  document.getElementsByClassName("display").innerHTML = 8;
+  document.getElementById("display").innerHTML = 8;
 };
 
 const nine = () => {
-  document.getElementsByClassName("display").innerHTML = 9;
+  document.getElementById("display").innerHTML = 9;
 };
 
 const zero = () => {
-  document.getElementsByClassName("display").innerHTML = 0;
+  document.getElementById("display").innerHTML = 0;
 };
